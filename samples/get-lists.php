aea58e2a3ae4197925fb68f44ad12c1ee95eb89b
adrian7 <adrian.silimon@yahoo.com>
2017-08-28 15:45:05 +0300

    added samples file

--- samples/get-lists.php
+++ samples/get-lists.php
@@ -8,9 +8,6 @@
 require_once ('../vendor/autoload.php');
 
 define('MC_API_KEY', 'ea400f0d078e0ddddf638e95e69f9b0f-us10');
-//define('MC_API_KEY', '-us10');
-
-echo "<pre style='font-size: 1.5em'>";
 
 $mc     = new \MailChimp\MailChimpAPI(MC_API_KEY);
 $result = $mc->get('lists', [
@@ -18,4 +15,4 @@ $result = $mc->get('lists', [
     'count'  => 10
 ]);
 
-echo var_dump($result);
+var_dump($result);
